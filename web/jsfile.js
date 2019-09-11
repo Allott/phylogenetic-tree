d6efe6adf35fa9a4b58c798bbf7409e1c25cefeb
Daniel Allott <36487931+Allott@users.noreply.github.com>
2019-09-11 14:57:26 +0100

    final JS

--- web/jsfile.js
+++ web/jsfile.js
@@ -1,19 +1,19 @@
 let objectContainer = document.querySelector('a-scene')
 let treeContainer = document.querySelector('#tree-container')
 
-function drawNode(container, position, colour,  text, parentPosition) {
+function drawNode(container, position, colour, colour2,  name, info, parentPosition) {
   let sphere = document.createElement('a-sphere')
-  sphere.setAttribute('node', '')
+  sphere.setAttribute('node', 'name: ' + name + "; info:" + info)
   sphere.setAttribute('position', position)
   sphere.setAttribute('color', colour)
-  sphere.setAttribute('value', text)
-  drawLine(container, position, parentPosition)
+  sphere.setAttribute('radius', 0.3)
+  drawLine(container, position, parentPosition, colour2)
   container.appendChild(sphere)
 }
 
-function drawLine(container, position1, position2) {
+function drawLine(container, position1, position2, colour) {
   let line = document.createElement('a-entity')
-  line.setAttribute('line', "start: " + position1 + "; end: " + position2 + "; colour: #ff0")
+  line.setAttribute('line', "start: " + position1 + "; end: " + position2 + "; color: " + colour)
   container.appendChild(line)
 }
 
@@ -29,29 +29,53 @@ function loadFile(filePath) {//extra
 }
 
 function drawTree(filePath, container) {
+  
+  while (container.firstChild) {
+    container.removeChild(container.firstChild)
+}
+  
   var tree = JSON.parse(loadFile(filePath))
   
   for (var i = 0; i < tree.length; i++) {
+    
     drawNode(container, 
             tree.nodes[i].position,
             tree.nodes[i].colour,
-            tree.nodes[i].text,
+            tree.nodes[i].colour2,
+            tree.nodes[i].name,
+            tree.nodes[i].distance,
             tree.nodes[tree.nodes[i].parentID].position)
   }
 }
 
-drawTree('input.json', treeContainer)
-
-
-
-//future stuff
 var toggle = function() {
-  //button for future use loading files
+  var m = document.getElementById("selectMenu")
+  var filename = m.options[m.selectedIndex].value;
+  drawTree(filename + '.json', treeContainer)
 }
 
-let hand = document.querySelector('a-entity')
+//let hand = document.querySelector('a-entity')//what does this even do
+
+
 document.querySelector('a-entity').addEventListener('trackpadchanged', function (evt) {
-  //button tracking on controller
+  
+  var transform = document.querySelector('#interface-transform')
+  var transform2 = document.querySelector('#camera-container')
+  
+  var newposition = transform.getAttribute('position')
+  //newposition  += transform2.getAttribute('position')
+  var newrotation = transform.getAttribute('rotation')
+  //newrotation += transform2.getAttribute('rotation')
+  var interCon = document.querySelector('#interface-container')
+  
+  interCon.setAttribute('rotation', newrotation)
+  //interCon.object3D.rotation.x =  0
+  //interCon.object3D.rotation.z =  0
+  
+  interCon.setAttribute('position', newposition)
+  interCon.object3D.position.y += -2 
+  interCon.object3D.position.x += 1 
+  
 })
 
 
